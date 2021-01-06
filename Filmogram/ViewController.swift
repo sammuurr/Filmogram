45d1acda9e2dfe7cec6ca0d94ba014043446d730
Samur Quseinov <sammuurr@gmail.com>
2021-01-07 00:06:05 +0300

    Initial Commit

---
+++ Filmogram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Filmogram
+//
+//  Created by ADMIMN on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
